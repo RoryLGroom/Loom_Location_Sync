34bf9293571c68d05081ab9f236f65394eb404ac
RoryLGroom <groomro@oregonstate.edu>
2026-02-10 22:08:28 -0800

    Multiple endpoints defined for hologram

--- src/example.py
+++ src/example.py
@@ -9,22 +9,27 @@ client = MongoClient(uri)
 
 
 try:
-    #Hologram - Fetch 
-    #retrieve lists of deviceswith one API call
-    hologram_url = "https://dashboard.hologram.io/api/1/devices/?orgid=30237"
+    #Hologram
+    #returns first 50 devices with location data. use the last device id in the query to tell 
+    #api where it left off
+    hologram_url_location = "https://dashboard.hologram.io/api/1/devices/locations?orgid=30237"
+
+    # will return all devices including last connected information. Will use this 
+    #to keep track of 
+    hologram_url_devices = "https://dashboard.hologram.io/api/1/devices?orgid=30237"
     #encode credentials using base64
     encoded_credentials = base64.b64encode(config.credentials.encode('utf-8')).decode('utf-8')
     #create header with hologram creds
     headers = {
     'Authorization': f'Basic {encoded_credentials}'
     }
-    response = requests.get(hologram_url, headers= headers)
+    response = requests.get(hologram_url_location, headers= headers)
 
     print(response.text)
 
     #Mongo
     #check devices with matching ID, if location doesnt exist, update
-    query = { "DeviceID": {"$exists": True}, "Location": {"$exists": False}}
+    query = {"DeviceID": {"$exists": True}, "Location": {"$exists": False}}
     """
 
     listOfDatabases= client.list_database_names()
