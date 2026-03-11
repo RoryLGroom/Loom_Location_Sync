7bb10453c64aa83e201a55d2718dde6746ba66c0
RoryLGroom <groomro@oregonstate.edu>
2026-03-11 12:52:07 -0700

    Update install script for virtual environment setup

--- install.sh
+++ install.sh
@@ -4,14 +4,20 @@
 set -e
 # make sure everything is updated
 sudo apt update
-# whatever the top file of the 
-PROJECT_DIR=home/rory/Desktop/Test
-cd $PROJECT_DIR
+# make sure we have what we need to create virtual environment
+sudo apt install -y python3 python3-venv python3-pip
+# detects where the install script lives, and uses that for the location where we want to install. 
+PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$PROJECT_DIR"
+# create virtual environment for intalling dependencies
 python3 -m venv .venv
+# activate the virtual environment
 source .venv/bin/activate
+# install the requiremnts in the local repo
 pip install -r requirements.txt
+# change permissions for bash script to be ran as an executable
 chmod +x script.sh
 
-echo "Please import config file"
-echo "Please set up crontab with 0 0,12 * * * /path/to/script.sh >> /path/to/cron.log 2>&1"
+echo "Please import config file, either by creating one or using scp from another device with the file."
+echo "Please set up crontab with 0 0,12 * * * $PROJECT_DIR/script.sh >> $PROJECT_DIR/cron.log 2>&1"
 
