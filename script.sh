452904e68ddcfb6df1dc41ae2e1c7f2ce8ae539c
RoryLGroom <groomro@oregonstate.edu>
2026-03-11 13:17:40 -0700

    Fix path to example.py in script.sh

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ set -x
 # locates directory where script.sh lives
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # runs python script, and stores output in script_output.txt
-$PROJECT_DIR/.venv/bin/python example.py >> $PROJECT_DIR/script_output.txt 2>&1
+$PROJECT_DIR/.venv/bin/python src/example.py >> $PROJECT_DIR/script_output.txt 2>&1
