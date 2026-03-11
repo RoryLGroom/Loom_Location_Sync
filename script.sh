354402627979930e3258d6757015746775c9f552
RoryLGroom <groomro@oregonstate.edu>
2026-03-11 13:01:45 -0700

    Refactor script.sh to improve directory handling
    Updated script to use dynamic project directory and virtual environment path.

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+# outputs commands in the cron log
 set -x
-cd /home/rory/Desktop/Loom_Map
-source .venv/bin/activate
-cd /home/rory/Desktop/Loom_Map/src
-python3 example.py >> script_output.txt 2>&1
+# locates directory where script.sh lives
+PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
+# runs python script, and stores output in script_output.txt
+$PROJECT_DIR/.venv/bin/python example.py >> $PROJECT_DIR/script_output.txt 2>&1
