174def70e60ca180efd6be98e6bb36e7c386eb1d
rory <rory@Debian.myguest.virtualbox.org>
2026-03-11 23:29:31 -0700

    Added timestamps for script outputs

--- script.sh
+++ script.sh
@@ -5,4 +5,7 @@ set -x
 # locates directory where script.sh lives
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # runs python script, and stores output in script_output.txt
-$PROJECT_DIR/.venv/bin/python src/example.py >> $PROJECT_DIR/script_output.txt 2>&1
+OUTPUT_RES=$($PROJECT_DIR/.venv/bin/python src/example.py) 
+TIME=$(date +"%Y-%m-%d_%H-%M-%S")
+
+echo -e "$TIME\n$OUTPUT_RES" >> $PROJECT_DIR/script_output.txt 2>&1
