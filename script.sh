9587ee0fd1791e5df85b601bec38e35f6d6f06f9
rory <rory@Debian.myguest.virtualbox.org>
2026-03-12 00:14:11 -0700

    fixed error with relative path in script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ set -x
 # locates directory where script.sh lives
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # runs python script, and stores output in script_output.txt
-OUTPUT_RES=$($PROJECT_DIR/.venv/bin/python src/example.py) 
+OUTPUT_RES=$($PROJECT_DIR/.venv/bin/python $PROJECT_DIR/src/example.py) 
 TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 
 echo -e "$TIME\n$OUTPUT_RES" >> $PROJECT_DIR/script_output.txt 2>&1
